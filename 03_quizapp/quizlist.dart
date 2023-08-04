6cb6203822faa20702c9ed9b984b53b2baa82da1
プログラミングコース <124702592+programing-course@users.noreply.github.com>
2023-08-04 11:33:49 +0900

    Create quizlist.dart

---
+++ 03_quizapp/quizlist.dart
@@ -0,0 +1,42 @@
+List<Map<String, dynamic>> quizlist = [
+  {
+    "question": "「カルピス」の名前の由来は何でしょう？", //問題文
+    "answer1": "カルシウム（calcium）とサルピス（salpis）", //選択肢１
+    "answer2": "カルボン酸（carboxylic acid）とピッチ（pitch）", //選択肢２
+    "answer3": "カロリー（calorie）とピストル（pistol）", //選択肢３
+    "answer4": "カルテット（quartet）とピアノ（piano）", //選択肢４
+    "correct": 1 //回答番号
+  },
+  {
+    "question": "キリンの舌の長さはどれくらいでしょう？",
+    "answer1": "25cm",
+    "answer2": "45cm",
+    "answer3": "65cm",
+    "answer4": "85cm",
+    "correct": 2
+  },
+  {
+    "question": "ニンジンは元々どの色だった？",
+    "answer1": "赤",
+    "answer2": "灰",
+    "answer3": "緑",
+    "answer4": "紫",
+    "correct": 4
+  },
+  {
+    "question": "ビール消費量世界1位のチェコでは、1年で1人あたり、ビール瓶何本分を消費するでしょう？",
+    "answer1": "50本",
+    "answer2": "100本",
+    "answer3": "300本",
+    "answer4": "600本",
+    "correct": 3
+  },
+  {
+    "question": "ペンギンの中で最も早く泳ぐことができるジェンツーペンギンの移動速度は？",
+    "answer1": "時速10km",
+    "answer2": "時速30km",
+    "answer3": "時速60km",
+    "answer4": "時速100km",
+    "correct": 2
+  },
+];
